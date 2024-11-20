76b0e4bad81619bc7894c3b0a78195cd541959c4
Sean Dooley <s.dooley@ctidigital.com>
2024-11-20 10:09:09 +0000

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Sean Dooley on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
